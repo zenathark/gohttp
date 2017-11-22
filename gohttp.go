3005b77592047ea0957f42d347b2e0a3c55c19de
Zenathark <jcgalanh@gmail.com>
2017-11-21 22:53:19 -0600

    Add first token, not checking

--- gohttp.go
+++ gohttp.go
@@ -1,54 +1,140 @@
 package gohttp
 
 import (
-// "fmt"
+	"regexp"
+	// "unicode/utf8"
 )
 
-type tokenType int
+// TokenType is the general type of token IDs
+type TokenType int
 
+// Types of tokens
 const (
-	DIGIT tokenType = iota
+	DIGIT TokenType = iota
 	ALPHA
+	CRLF
 )
 
-type simpleToken struct {
-	id tokenType
+// SimpleToken represents a token with only an ID.
+// For terminal symbols such as ( )
+type SimpleToken struct {
+	id TokenType
 }
 
-type dataToken struct {
-	id    tokenType
+// DataToken represents a token with its watching string
+// for undetermined terminals as ALHA
+type DataToken struct {
+	id    TokenType
 	value string
 }
 
+// Token interface only returns its type
 type Token interface {
-	getId() tokenType
+	getID() TokenType
 }
 
+// TokenWithData is a token that carries on its token hit
 type TokenWithData interface {
 	getValue() string
 }
 
-func (t *simpleToken) GetId() tokenType {
+// GetID returns the ID of the SimpleToken
+func (t *SimpleToken) GetID() TokenType {
 	return t.id
 }
 
-func (t *dataToken) GetId() tokenType {
+// GetID returns the ID of the DataToken
+func (t *DataToken) GetID() TokenType {
 	return t.id
 }
 
-func (t *dataToken) GetValue() string {
+// GetValue returns the matching string of the DataToken
+func (t *DataToken) GetValue() string {
 	return t.value
 }
 
-func NewToken(id tokenType) *simpleToken {
-	t := new(simpleToken)
+// NewToken creates a new simple token
+func NewToken(id TokenType) *SimpleToken {
+	t := new(SimpleToken)
 	t.id = id
 	return t
 }
 
-func NewDataToken(id tokenType, data string) *dataToken {
-	t := new(dataToken)
+// NewDataToken represents a token with its matching string
+func NewDataToken(id TokenType, data string) *DataToken {
+	t := new(DataToken)
 	t.id = id
 	t.value = data
 	return t
+}
+
+// SimpleTokenMatcher is the basic Interface of a token matcher.
+type SimpleTokenMatcher interface {
+	// Match must return true if the following token matches its rule
+	Match() bool
+	// Gets the token that matches the rule, produces an error if the following
+	// token is not of this type
+	GetToken() (SimpleToken, error)
+}
+
+// DataTokenMatcher is the basic Interface of a data token matcher.
+type DataTokenMatcher interface {
+	// Match must return true if the following token matches its rule
+	Match() bool
+	// Gets the token that matches the rule, produces an error if the following
+	// token is not of this type
+	GetToken() (DataToken, error)
+}
+
+// ------------------- Protocol definition HTTP 1.0-----------------------------
+
+type tokenRegex int
+
+const (
+	crlf tokenRegex = iota
+	alpha
+)
+
+var terminalSymbols = map[tokenRegex]string{
+	crlf:  "\r\n",
+	alpha: "a",
+}
+
+var regexSymbols = map[tokenRegex]*regexp.Regexp{
+	crlf:  regexp.MustCompile(terminalSymbols[crlf]),
+	alpha: regexp.MustCompile(terminalSymbols[alpha]),
+}
+
+// HTTPTokenizer digests a http request message
+type HTTPTokenizer struct {
+	Message string
+	cursor  int
+}
+
+// NewHTTPTokenizer is the basic contructor
+func NewHTTPTokenizer(request string) *HTTPTokenizer {
+	tok := new(HTTPTokenizer)
+	tok.Message = request
+	tok.cursor = 0
+	return tok
+}
+
+// NextToken gets the next valid token of the request
+func (tok *HTTPTokenizer) NextToken() *DataToken {
+	for _, value := range regexSymbols {
+		pos := value.FindStringIndex(tok.Message[tok.cursor:])
+		if pos != nil && pos[0] == 0 {
+			return NewDataToken(CRLF, tok.Message[tok.cursor:])
+		}
+	}
+	// for i := tok.cursor + 1; i < utf8.RuneCountInString(tok.Message[tok.cursor:]); i++ {
+	//	window := tok.Message[tok.cursor:i]
+	//	for _, value := range regexSymbols {
+	//		pos := value.FindStringIndex(window)
+	//		if pos != nil && pos[0] == 0 {
+	//			return NewDataToken(CRLF, window)
+	//		}
+	//	}
+	// }
+	return nil
 }
--- gohttp_test.go
+++ gohttp_test.go
@@ -2,19 +2,31 @@ package gohttp_test
 
 import (
 	"github.com/zenathark/gohttp"
+	. "gopkg.in/check.v1"
 	"testing"
 )
 
-func TestNewToken(t *testing.T) {
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestNewToken(c *C) {
 	k := gohttp.NewToken(gohttp.DIGIT)
-	if k.GetId() != gohttp.DIGIT {
-		t.Error("Expected token as DIGIT")
-	}
+	c.Assert(k.GetID(), Equals, gohttp.DIGIT)
 }
 
-func TestNewDataToken(t *testing.T) {
+func (s *MySuite) TestNewDataToken(c *C) {
 	k := gohttp.NewDataToken(gohttp.DIGIT, "3")
-	if k.GetId() != gohttp.DIGIT || k.GetValue() != "3" {
-		t.Error("Expected token as DIGIT")
-	}
+	c.Assert(k.GetID(), Equals, gohttp.DIGIT)
+	c.Assert(k.GetValue(), Equals, "3")
+}
+
+func (s *MySuite) TestNextToken(c *C) {
+	t := gohttp.NewHTTPTokenizer("a")
+	token := t.NextToken()
+	c.Assert(t.Message, Equals, "a")
+	c.Assert(token, Not(Equals), (*gohttp.DataToken)(nil))
+	c.Assert(token, Equals, gohttp.CRLF)
 }
